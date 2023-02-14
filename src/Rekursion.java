2f12a9a78a28f579c577dcf6de1c952c0f16821e
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-02-14 09:46:30 +0100

    Fibonacci Methode ergänzt.

--- src/Rekursion.java
+++ src/Rekursion.java
@@ -1,5 +1,10 @@
 public class Rekursion {
 
+    public static int fibonacci(int n) {
+        if (n <= 2) return 1;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
     private static int fakultät(int n) {
         if (n == 0) return 1;
         return n * fakultät(n-1);
@@ -8,5 +13,6 @@ public class Rekursion {
     public static void main(String[] args) {
         int f = fakultät(7);
         System.out.println(f);
+        System.out.println( fibonacci(20) );
     }
 }
