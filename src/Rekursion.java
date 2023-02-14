1ddfc192518b6267e0da930ca195d08bd2ba8e89
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-02-14 10:53:19 +0100

    Fibonacci Methode ergänzt mit Optimierung durch ein Arrays, das die bereits berechneten Werte merkt.

--- src/Rekursion.java
+++ src/Rekursion.java
@@ -1,8 +1,20 @@
 public class Rekursion {
 
-    public static int fibonacci(int n) {
-        if (n <= 2) return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
+    private static long[] fibonacci_werte = new long[100];  // dieses Array hat lauter 0-Werte
+        //{ 0, 1, 1, 2, 3, 5, ... } // 100 Stück ..
+
+    private static long aufruf_zähler = 0;
+
+    public static long fibonacci(long n) {
+        aufruf_zähler++;
+
+        if (fibonacci_werte[ (int)n ] > 0)
+            return fibonacci_werte[ (int)n ];
+
+        long ergebnis = 1;
+        if (n > 2) ergebnis = fibonacci(n-1) + fibonacci(n-2);
+        fibonacci_werte[ (int)n ] = ergebnis;
+        return ergebnis;
     }
 
     private static int fakultät(int n) {
@@ -13,6 +25,11 @@ public class Rekursion {
     public static void main(String[] args) {
         int f = fakultät(7);
         System.out.println(f);
-        System.out.println( fibonacci(20) );
+
+        long zeit = System.currentTimeMillis();
+        System.out.println( fibonacci(50) );
+        zeit = System.currentTimeMillis() - zeit;
+        System.out.println("Die Berechnung hat " + zeit + "ms gedauert.");
+        System.out.println("Die Methode wurde " + aufruf_zähler + " Mal aufgerufen");
     }
 }
